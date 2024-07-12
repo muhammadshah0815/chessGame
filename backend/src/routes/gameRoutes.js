8d9bcd1ced91a2d7e72817232f5ca6c017206fb3
muhammadshah0815 <muhammadshah0815@gmail.com>
2024-07-13 01:28:49 +0500

    ensure backend updates

--- backend/src/routes/gameRoutes.js
+++ backend/src/routes/gameRoutes.js
@@ -32,7 +32,27 @@ router.post('/move/:id', async (req, res) => {
     const piece = game.board[startRow][startCol];
     game.board[startRow][startCol] = '';
     game.board[endRow][endCol] = piece;
+
+    // Update taken pieces
+    if (game.board[endRow][endCol] !== '') {
+      if (piece === piece.toUpperCase()) {
+        game.blackTaken.push(game.board[endRow][endCol]);
+      } else {
+        game.whiteTaken.push(game.board[endRow][endCol]);
+      }
+    }
+
+    // Update turn
     game.turn = game.turn === 'white' ? 'black' : 'white';
+
+    // Update moved state for king and rooks
+    if (piece.toLowerCase() === 'k') {
+      game.kingMoved[game.turn === 'white' ? 'black' : 'white'] = true;
+    }
+    if (piece.toLowerCase() === 'r') {
+      if (startCol === 0) game.rookMoved[game.turn === 'white' ? 'black' : 'white'].queenside = true;
+      if (startCol === 7) game.rookMoved[game.turn === 'white' ? 'black' : 'white'].kingside = true;
+    }
 
     await game.save();
     res.json(game);
